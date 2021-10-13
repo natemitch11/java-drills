551061ece475057b302df98ee65aff37542abb82
natemitch11 <nate.mitch11@gmail.com>
2021-10-13 16:57:12 -0600

    Finalized StarWars portion

--- src/starwars/CharacterProfile.java
+++ src/starwars/CharacterProfile.java
@@ -1,18 +1,56 @@
 package starwars;
 
 public class CharacterProfile {
-    private String firstName = "Qui-Gon";
-    private String lastName = "Jinn";
-    private int birthYearBBY = 80;
-    private String homeWorld = "Coruscant";
-    private float heightInMeters = 1.93f;
-    private int weightInKg = 85;
-
     public static void main(String[] args){
-        //System.out.printf("Hello There! My name is %s %s. I was born in the year %s BBY.");
+        ConditionalLove love = new ConditionalLove();
         Shopping shopping = new Shopping();
+
         shopping.setPurse(150f);
-        shopping.getPurse();
         shopping.shopping();
+
+        love.chooseLightsaber("black", "purple");
+        love.catchARide("millennium falcon");
+        love.buyFoodItem("Womp Rat", 4);
+        love.buyFoodItem("Rancor", 100);
+        love.buyDrink("Jawa Juice", 1.99f, 36);
+        love.buyDrink("Blue Milk", 1.99f, 22);
+        love.buyDrink("Green Milk", 0.99f, 22);
+
+        Character quiGon = new Character(firstName, lastName, birthYearBBY, homeWorld, heightInMeters, weightInKg);
+        System.out.printf("%s %s, was born %sBBY on the planet %s.%n", quiGon.firstName, quiGon.lastName,
+                quiGon.birthYear, quiGon.homeWorld);
     }
+
+    //Character Fields
+    private static String firstName = "Qui-Gon";
+    private static String lastName = "Jinn";
+    private static int birthYearBBY = 80;
+    private static String homeWorld = "Coruscant";
+    private static float heightInMeters = 1.93f;
+    private static int weightInKg = 85;
+
+    private static class Character{
+        String firstName;
+        String lastName;
+        int birthYear;
+        String homeWorld;
+        float heightInMeters;
+        int weightInKg;
+
+        public Character(String firstName, String lastName, int birthYear, String homeWorld, float heightInMeters,
+                                             int weightInKg)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthYear = birthYear;
+            this.homeWorld = homeWorld;
+            this.heightInMeters = heightInMeters;
+            this.weightInKg = weightInKg;
+        }
+    }
+
+
+
+
+
 }
---
+++ src/starwars/ConditionalLove.java
@@ -0,0 +1,67 @@
+package starwars;
+import java.util.Random;
+
+public class ConditionalLove {
+    private Random rand = new Random();
+    private static class Food{
+        String name;
+        int price;
+        boolean recommended;
+    }
+    private Object createFood(String name, int price){
+        Food food = new Food();
+        food.name = name;
+        food.price = price;
+        food.recommended = rand.nextBoolean();
+        return food;
+    }
+    private static class Drink{
+        String name;
+        float price;
+        int sizeInOz;
+    }
+    private Object createDrink(String name, float price, int size){
+        Drink drink = new Drink();
+        drink.name = name;
+        drink.price = price;
+        drink.sizeInOz = size;
+        return drink;
+    }
+
+    public void chooseLightsaber(String handle, String color){
+        if (handle.equalsIgnoreCase("black") && color.equalsIgnoreCase("purple") ){
+            System.out.println("This one's mine!");
+        } else {
+            System.out.println("Pass");
+        }
+    }
+    public void catchARide (String shipname){
+        if (shipname.equalsIgnoreCase("Millennium Falcon")){
+            System.out.println("Let's Go!");
+        } else {
+            System.out.println("I better hide somewhere!");
+        }
+    }
+    public void buyFoodItem (String item, int price){
+        Object food = createFood(item, price);
+        int foodPrice = ((Food) food).price;
+        boolean foodRec = ((Food) food).recommended;
+
+        if (foodPrice < 5 && foodRec == true){
+            System.out.printf("I think I'll try this %s!%n", item);
+        } else {
+            System.out.printf("I think I'll pass on eating %s%n", item);
+        }
+    }
+    public void buyDrink (String name, float price, int size){
+        Object drink = createDrink(name, price, size);
+        float drinkPrice = ((Drink) drink).price;
+        int drinkSize = ((Drink) drink).sizeInOz;
+
+        if ( drinkPrice < 1 || drinkSize > 24){
+            System.out.printf("I think I'll buy this %s!%n", name);
+        } else {
+            System.out.println("Nah, I'll just finish this drink");
+        }
+    }
+}
